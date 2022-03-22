8af7bc5ca6b07fb22fa9a9330db6af6d328509a2
beatricekallen <bkallen90@gmail.com>
2022-03-22 16:43:58 -0400

    adds 5-day forecast functionality, search history functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,13 @@ var cityDateEl = document.querySelector("#city-date");
 var cityWeatherIconEl = document.querySelector("#weather-icon");
 var cityWeatherTempEl = document.querySelector("#city-temp");
 var cityHumidityEl = document.querySelector("#city-humidity");
-var cityWindSpeed = document.querySelector("#wind-speed");
-var cityUvIndex = document.querySelector("#uv-index");
+var cityWindSpeedEl = document.querySelector("#wind-speed");
+var cityUvIndexEl = document.querySelector("#uv-index");
 var cityContainerEl = document.querySelector("#city-container-current");
 var submitButtonEl = document.querySelector("#button");
+var forecastContainerEl = document.querySelector("#forecast");
+var forecastContainerHeadingEl = document.querySelector("#forecast-heading");
+var searchHistoryButtonsEl = document.querySelector(".btn-secondary");
 var citiesArray = [];
 
 // form handler function: needs to prevent default and run getWeather function on submission
@@ -23,6 +26,7 @@ var formSubmitHandler = function (event) {
     alert("Please enter a city name.");
   }
   storeCities(city);
+  loadCities();
 };
 
 // getCityName function: needs to convert city name into coordinates
@@ -33,7 +37,7 @@ var getCityCoord = function (city) {
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data);
+          // console.log(data);
           lat = data[0].lat;
           long = data[0].lon;
           cityNameEl.textContent = data[0].name;
@@ -50,7 +54,7 @@ var getCityCoord = function (city) {
 
 // getWeather function: needs to get info from the API based on user input city name
 var getWeather = function (latitude, longitude) {
-  var cityApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=a0c2e4175139a11ef4d0913ba3bef922`;
+  var cityApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=a0c2e4175139a11ef4d0913ba3bef922&units=imperial`;
 
   fetch(cityApiUrl)
     .then(function (response) {
@@ -71,50 +75,86 @@ var getWeather = function (latitude, longitude) {
 // displayWeather function: needs to display info from the API call to the page. Needs to display current conditions as well as five-day forecast (or should this be separate function?)
 var displayWeather = function (data) {
   console.log(data);
-  console.log(cityDateEl);
-  // for (var i = 0; i < data.length; i++) {
-  // cityContainerEl.textContent = "";
+  forecastContainerEl.textContent = "";
 
-  cityDateEl.textContent = data.current.dt;
-  cityWeatherIconEl.textContent = data.current.weather.icon;
-  cityWeatherTempEl.textContent = data.current.temp;
-  cityHumidityEl.textContent = data.current.humidity;
-  cityWindSpeed.textContent = data.current.wind_speed;
-  cityUvIndex.textContent = data.current.uvi;
+  var utcDate = parseInt(data.current.dt) + parseInt(data.timezone_offset);
+
+  cityDateEl.textContent = new Date(parseInt(utcDate));
+  cityWeatherIconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png'/>`;
+  cityWeatherTempEl.textContent = `Current temperature: ${data.current.temp}°F`;
+  cityHumidityEl.textContent = `Current humidity: ${data.current.humidity}%`;
+  cityWindSpeedEl.textContent = `Current windspeed: ${data.current.wind_speed} mph`;
+  cityUvIndexEl.textContent = `Current UV index: ${data.current.uvi}`;
+
+  if (data.current.uvi <= 4) {
+    cityUvIndexEl.setAttribute("class", "uv-low");
+  } else if (data.current.uvi <= 7) {
+    cityUvIndexEl.setAttribute("class", "uv-moderate");
+  } else cityUvIndexEl.setAttribute("class", "uv-high");
+
+  forecastContainerHeadingEl.textContent = "5-Day Forecast";
+
+  for (var i = 0; i < 5; i++) {
+    var forecastUtcDate = data.daily[i].dt;
+    var forecastDayEl = document.createElement("div");
+    var forecastDateEl = document.createElement("p");
+    var forecastIconEl = document.createElement("p");
+    var forecastTempEl = document.createElement("p");
+    var forecastWindSpeedEl = document.createElement("p");
+    var forecastHumidityEl = document.createElement("p");
+
+    forecastDateEl.textContent = Date(forecastUtcDate);
+    forecastIconEl.innerHTML = `<img src='http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png'/>`;
+    forecastTempEl.textContent = `Temp: ${data.daily[i].temp.day}°F`;
+    forecastWindSpeedEl.textContent = `Windspeed: ${data.daily[i].wind_speed} mph`;
+    forecastHumidityEl.textContent = `UV index: ${data.daily[i].humidity}%`;
+
+    forecastDayEl.setAttribute("class", "forecast-days");
+
+    forecastDayEl.appendChild(forecastDateEl);
+    forecastDayEl.appendChild(forecastIconEl);
+    forecastDayEl.appendChild(forecastTempEl);
+    forecastDayEl.appendChild(forecastWindSpeedEl);
+    forecastDayEl.appendChild(forecastHumidityEl);
+    forecastContainerEl.appendChild(forecastDayEl);
+  }
 };
-// };
 
 // storeCities function: needs to save search history to local storage
 var storeCities = function (city) {
-  console.log(city);
   citiesArray = JSON.parse(localStorage.getItem("search-history")) || [];
   citiesArray.push(city);
   localStorage.setItem("search-history", JSON.stringify(citiesArray));
-
-  // var newCity = {
-  //   city: cityInputEl,
-  // };
-
-  // var citiesArray = JSON.parse(localStorage.getItem("cities")) || [];
-  // citiesArray.push(newCity);
-  // var citiesString = JSON.stringify(citiesArray);
-  // localStorage.setItem("cities", citiesString);
 };
 
 var loadCities = function () {
   var storedCities = JSON.parse(localStorage.getItem("search-history"));
 
   for (var i = 0; i < storedCities.length; i++) {
-    console.log("working");
     var searchHistoryButtonEl = document.createElement("button");
     searchHistoryButtonEl.textContent = storedCities[i];
     var searchHistoryContainerEl = document.querySelector("#search-history");
     searchHistoryButtonEl.setAttribute("data-search", storedCities[i]);
+    searchHistoryButtonEl.setAttribute(
+      "class",
+      "btn btn-secondary btn-block p-2"
+    );
+    searchHistoryButtonEl.setAttribute("type", "submit");
+    searchHistoryButtonEl.setAttribute("id", "search-history-button");
     searchHistoryContainerEl.appendChild(searchHistoryButtonEl);
   }
 };
 
-loadCities();
+$(document).ready(loadCities);
 
 //event listener for submit form
 submitButtonEl.addEventListener("click", formSubmitHandler);
+
+document
+  .querySelector("#search-history")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var city = event.target.dataset.search;
+    console.log(city);
+    getCityCoord(city);
+  });
